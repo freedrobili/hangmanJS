163622e98c7d80434f65cd182ffc602d3839808d
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2024-01-10 20:09:47 +0300

    v1.0

---
+++ src/Controller.js
@@ -0,0 +1,21 @@
+import {
+    startGame,
+    informationOutput,
+    showGameOutput
+} from './View.js';
+
+import {
+    btn_nextInfo,
+    btn_nextShowGame,
+    btn_checkLetter,
+    btn_replayGame,
+    checkLetter
+} from "./Model.js";
+
+
+startGame();
+
+btn_nextInfo.addEventListener("click", informationOutput);
+btn_nextShowGame.addEventListener("click", showGameOutput);
+btn_checkLetter.addEventListener("click", checkLetter);
+btn_replayGame.addEventListener("click", showGameOutput);
